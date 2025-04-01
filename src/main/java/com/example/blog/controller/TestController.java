0346091e3670647277cef6de5ab4aae07b409047
kdhdd <kdso10@naver.com>
2025-04-01 13:04:03 +0900

    TestController 추가

---
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.blog.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
